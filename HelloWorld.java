757306df0fa5456bbef10260b15787efcc17a9eb
MohamedAdam <Mohamed.gmail>
2016-12-31 12:55:44 +0300

    this is a new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -21,5 +21,5 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, world");
+        System.out.println("welcome to the class");
 }
